b89e24e782684e8719ed5934eeb6760bdf4b2ef8
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-01-18 20:50:43 +0900

    パスワードの最小文字設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
   validates :email, length: { maximum: 255 }
   validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
