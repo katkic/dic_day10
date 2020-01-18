33f7cf6544fcb050d51295ad9f50443d34e09bc8
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-01-18 20:46:14 +0900

    文字数のバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true
+  validates :name, length: { maximum: 30 }
   validates :email, presence: true
+  validates :email, length: { maximum: 255 }
 end
