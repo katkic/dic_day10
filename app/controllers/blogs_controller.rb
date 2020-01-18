2c77fc7c4e7740a5e07b23bf9aa16e4c6f7dc3b3
yusuke nakano <yuusukechrono@gmail.com>
2020-01-18 12:33:09 +0000

    削除機能作成

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,6 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update, :destroy
+
   def index
     @blogs = Blog.all
   end
@@ -25,10 +27,18 @@ class BlogsController < ApplicationController
     end
   end
 
+  def destroy
+    @blog.destroy
+    redirect_to blogs_path, notice:"ブログを削除しました！"
+  end
+
   private
 
   def blog_params
     params.require(:blog).permit(:title, :content)
   end
 
+  def set_blog
+    @blog = Blog.find(params[:id])
+  end
 end
