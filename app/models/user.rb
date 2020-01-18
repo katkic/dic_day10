45b0a38a3bda76313e455e1aec527816428a9525
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-01-18 20:48:56 +0900

    バリデーション前に小文字に変換

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   validates :name, presence: true
   validates :name, length: { maximum: 30 }
+  before_validation { email.downcase! }
   validates :email, presence: true
   validates :email, length: { maximum: 255 }
   validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
