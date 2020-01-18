a478573ed232dbf7a96ef4c2d1b2d31f765584f3
yusuke nakano <yuusukechrono@gmail.com>
2020-01-18 12:41:16 +0000

    最新

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,5 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update, :destroy
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
 
   def index
     @blogs = Blog.all
@@ -18,6 +18,7 @@ class BlogsController < ApplicationController
     else
       render :edit
     end
+  end
 
   def create
     Blog.create(blog_params)
